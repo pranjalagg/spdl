c0278b76743e522a718f4de43fab03381445d87e
Pranjal Aggarwal <54495980+pranjalagg@users.noreply.github.com>
2024-07-31 01:01:55 -0600

    Name Sanitization For Playlists

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ CUSTOM_HEADER = {
     'Origin': 'https://spotifydown.com',
 }
 
-TRACKNAME_REGEX = re.compile(r"[<>:\"\/\\|?*]")
+NAME_SANITIZE_REGEX = re.compile(r"[<>:\"\/\\|?*]")
 
 @dataclass(init=True, eq=True, frozen=True)
 class Song:
@@ -63,7 +63,7 @@ def  get_track_info(link):
 
 def attach_cover_art(trackname, cover_art, outpath):
     # print(outpath)
-    trackname = re.sub(TRACKNAME_REGEX, "_", trackname)
+    trackname = re.sub(NAME_SANITIZE_REGEX, "_", trackname)
     audio_file = eyed3.load(os.path.join(outpath, f"{trackname}.mp3"))
 
     # https://stackoverflow.com/questions/38510694/how-to-add-album-art-to-mp3-file-using-python-3
@@ -80,7 +80,7 @@ def attach_cover_art(trackname, cover_art, outpath):
         # print(e)
 
 def save_audio(trackname, link, outpath):
-    trackname = re.sub(TRACKNAME_REGEX, "_", trackname)
+    trackname = re.sub(NAME_SANITIZE_REGEX, "_", trackname)
     if os.path.exists(os.path.join(outpath, f"{trackname}.mp3")):
         logging.info(f"{trackname} already exists in the directory ({outpath}). Skipping download!")
         print("\t This track already exists in the directory. Skipping download!")
@@ -134,8 +134,8 @@ def make_unique_song_objects(track_list, trackname_convention):
     for track in track_list:
         song_list.append(
             Song(
-                title=re.sub(TRACKNAME_REGEX, "_", track['title']),
-                artists=re.sub(TRACKNAME_REGEX, "_", track['artists']),
+                title=re.sub(NAME_SANITIZE_REGEX, "_", track['title']),
+                artists=re.sub(NAME_SANITIZE_REGEX, "_", track['artists']),
                 album=track['album'],
                 cover=track['cover'],
                 link=f"https://open.spotify.com/track/{track['id']}"
@@ -245,7 +245,10 @@ def remove_empty_files(outpath):
 
 def download_playlist_tracks(playlist_link, outpath, create_folder, trackname_convention, max_attempts=3):
     print("\nPlaylist link identified")
-    song_list_dict, playlist_name = get_playlist_info(playlist_link, trackname_convention)
+    song_list_dict, playlist_name_old = get_playlist_info(playlist_link, trackname_convention)
+    playlist_name = re.sub(NAME_SANITIZE_REGEX, "_", playlist_name_old)
+    if (playlist_name != playlist_name_old):
+        print(f'\n"{playlist_name_old}" is not a valid folder name. Using "{playlist_name}" instead.')
 
 
     if create_folder == True:
